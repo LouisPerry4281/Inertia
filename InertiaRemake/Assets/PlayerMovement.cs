f48d7da37a72082075688197179fcac1a9a9cd7c
LouisPerry4281 <louisperry4281@gmail.com>
2025-01-30 17:36:27 +0000

    Implemented Camera-Based Movement

--- InertiaRemake/Assets/PlayerMovement.cs
+++ InertiaRemake/Assets/PlayerMovement.cs
@@ -30,6 +30,28 @@ public class PlayerMovement : MonoBehaviour
         playerInput.x = xInput;
         playerInput.z = yInput;
 
-        characterController.Move(movementSpeed * playerInput * Time.deltaTime);
+        Vector3 cameraRelativeMovement = ConvertToCameraSpace(playerInput);
+        characterController.Move(movementSpeed * cameraRelativeMovement * Time.deltaTime);
+    }
+
+    Vector3 ConvertToCameraSpace(Vector3 vectorToRotate)
+    {
+        //Grab the camera forward and camera right from the main camera
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+
+        //Flatten the camera's y so it only indicates an xz plane
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        //Normalize the now lengthened vectors so they have a magnitude of 1
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        Vector3 cameraForwardZProduct = vectorToRotate.z * cameraForward;
+        Vector3 cameraRightXProduct = vectorToRotate.x * cameraRight;
+
+        Vector3 vectorRotatedToCameraSpace = cameraForwardZProduct + cameraRightXProduct;
+        return vectorRotatedToCameraSpace;
     }
 }
