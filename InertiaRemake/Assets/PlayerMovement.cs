4ec8b4389e3b714c932ad2f3c7f47afce9a94cac
LouisPerry4281 <louisperry4281@gmail.com>
2025-01-30 17:24:40 +0000

    Basic Movement
    Is still world space-based, will have to convert

---
+++ InertiaRemake/Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController characterController;
+
+    float xInput, yInput;
+    Vector3 playerInput;
+
+    [SerializeField] float movementSpeed;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+
+        playerInput.x = xInput;
+        playerInput.z = yInput;
+
+        characterController.Move(movementSpeed * playerInput * Time.deltaTime);
+    }
+}
